2d3b9bd55c61c494da73c1d548b5c10fe50686a4
KatrineStorgaard <katrine.storgaards@gmail.com>
2014-10-30 14:07:40 +0100

    Forsøg på Gui + lidt ændring af klassenavne

---
+++ src/gui/_Screen.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.JFrame;
+import java.awt.CardLayout;
+
+public class _Screen extends JFrame{
+	public _Screen() {
+		getContentPane().setLayout(new CardLayout(0, 0));
+	}
+
+}
