60f4fb57ed4f05156f1ed83a21c6d0525de9cd3f
KatrineStorgaard <katrine.storgaards@gmail.com>
2014-11-10 15:49:46 +0100

    CalendarWeek works!

--- src/logic/EventsController.java
+++ src/logic/EventsController.java
@@ -1,5 +1,16 @@
 package logic;
 
 public class EventsController {
+	
+	public void CreateEvent(){
+		shared.ObjectTranslator.getEvent();
+	}
 
+	public void EditEvent(){
+		shared.ObjectTranslator.getEvent();
+	}
+	
+	public void DeleteEvent(){
+		shared.ObjectTranslator.getEvent();
+	}
 }
